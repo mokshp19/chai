7b9dea9d14612192d3e15de25757367c3d81b21f
mokshp19 <mokshpatels@gmail.com>
2025-08-20 23:22:13 -0400

    chai.com

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+
+  function GayButton(){
+     return (
+    <button>
+      I'm a gay
+    </button>
+  );
+}
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <>
-      <div>
-        <a>
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a>
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Chai - Find Your Cup Of Tea</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          {count}
-        </button>
-        {/* <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p> */}
-      </div>
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <GayButton />
+      <img src="/cupoftea.png" className="logo" alt="couple logo" />
     </>
   )
 }
