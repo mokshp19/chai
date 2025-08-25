2821c9cc6f3127992bb8dca4eed7b804cbc06487
mokshp19 <mokshpatels@gmail.com>
2025-08-25 12:56:48 -0400

    Home page finished

--- src/App.tsx
+++ src/App.tsx
@@ -22,15 +22,21 @@ function App() {
       <h2>find your cup of tea</h2>
       <img src="/cup.png" className="cup" alt="cup logo" />
       </div>
-      <GayButton />
       <div>
       <img src="/couple.png" className="logo" alt="couple logo" />
       <div className="signup">
       <h3>Sign Up</h3>
       <h4>Name</h4>
+      <input type="text" placeholder="Name" />
       <h4>Phone Number</h4>
+      <input type="text" placeholder="Phone Number" />
       <h4>Email</h4>
+      <input type="text" placeholder="Email" />
+      <button className = "register">Sign Up</button>
       </div>
+     
+      
+      
       </div>
       
 
